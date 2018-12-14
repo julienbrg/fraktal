51526291da78f7014480d831f90fe04c0046a1e8
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-12-14 12:07:57 +0100

    Update directory linking

---
+++ src/containers/Fraktal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Fraktal extends Component {
+  render() {
+    return (
+      <div>
+        Fraktal App
+      </div>
+    );
+  }
+}
+
+export default Fraktal;
