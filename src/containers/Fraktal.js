8050da050dc88db9cc25fec6ac6bc2506b6b3521
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-12-14 13:23:56 +0100

    Add Components to App

--- src/containers/Fraktal.js
+++ src/containers/Fraktal.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+import Title from "../components/Title";
+import WelcomeBanner from "../components/Welcome";
+import MathUI from "../components/Math";
+import Button from '../components/Button';
 
 class Fraktal extends Component {
   render() {
     return (
-      <div>
-        Fraktal App
-      </div>
+      <Container>
+        <Row type="flex" align="middle">
+          <Col
+            xs={12}
+            sm={12}
+            md={{ size: "auto", offset: 2 }}
+            lg={{ size: "auto", offset: 2 }}
+          >
+            <Title />
+          </Col>
+        </Row>
+        <Row type="flex" align="middle">
+          <Col
+            xs={12}
+            sm={12}
+            md={{ size: "auto", offset: 2 }}
+            lg={{ size: "auto", offset: 2 }}
+          >
+            <MathUI />
+            <Button position="right"/>
+          </Col>
+          <Col xs={12} sm={12} md={3} lg={3}>
+            <WelcomeBanner />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
