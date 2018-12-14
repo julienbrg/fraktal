d6c5e135f3c23e031ea439bdcad65e95e2bec643
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-12-14 13:58:23 +0100

    Add Progress component to App

--- src/containers/Fraktal.js
+++ src/containers/Fraktal.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+import ReactGA from "react-ga";
 import Title from "../components/Title";
 import WelcomeBanner from "../components/Welcome";
 import MathUI from "../components/Math";
-import Button from '../components/Button';
+import Button from "../components/Button";
+import ProgressUI from "../components/Progress";
 
 class Fraktal extends Component {
+  componentDidMount() {
+    ReactGA.initialize("UA-130996010-1");
+  }
+
   render() {
     return (
       <Container>
@@ -27,7 +33,8 @@ class Fraktal extends Component {
             lg={{ size: "auto", offset: 2 }}
           >
             <MathUI />
-            <Button position="right"/>
+            <ProgressUI />
+            <Button position="right" />
           </Col>
           <Col xs={12} sm={12} md={3} lg={3}>
             <WelcomeBanner />
